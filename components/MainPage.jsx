bcd67789376c9eeae7672d0792ae913a891b29ab
Edwin Roosevelt <bedwinroosevelt@gmail.com>
2022-08-21 22:59:53 +0530

    fixed built errors

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -213,12 +213,12 @@ function MainPage() {
 			width="16"
 			height="16"
 			fill="currentColor"
-			class="bi bi-trash"
+			className="bi bi-trash"
 			viewBox="0 0 16 16"
 		>
 			<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
 			<path
-				fill-rule="evenodd"
+				fillRule="evenodd"
 				d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
 			/>
 		</svg>
@@ -434,7 +434,7 @@ function MainPage() {
 
 				{table && (
 					<>
-						<h1 className="fs-2 p-3">Teacher's table</h1>
+						<h1 className="fs-2 p-3">Teacher&apos;s table</h1>
 						<div className=" p-3" style={{ overflowY: "scroll", height: "500px" }}>
 							{Object.entries(table.teacherTable).map((teacher) => (
 								<table key={teacher[0]} className="table table-bordered bg-light">
@@ -477,7 +477,7 @@ function MainPage() {
 								</table>
 							))}
 						</div>
-						<h1 className="fs-2 p-3">Class's table</h1>
+						<h1 className="fs-2 p-3">Class&apos;s table</h1>
 						<div className=" p-3" style={{ overflowY: "scroll", height: "500px" }}>
 							{Object.entries(table.classTable).map((classId) => (
 								<table key={classId} className="table table-bordered bg-light">
